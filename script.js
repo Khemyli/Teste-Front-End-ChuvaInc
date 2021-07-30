c4dd149313336b1b38ea3f5ecf08f7e1145e851a
Khemyli Mendonça <65932172+Khemyli@users.noreply.github.com>
2021-07-30 16:36:56 -0300

    third commit
    adicionando as div que somem e aparecem

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 	var bnt = document.querySelector('#mostrar');
 	var hidden = document.querySelector('.hidden');
+	var bntform = document.querySelector('#botao-discussoes')
+	var form = document.querySelector('#form')
+	var discussoes = document.querySelector('#discussoes')
+	var bntenviar = document.querySelector('#botao-form')
+	
 
 
 bnt.addEventListener('click', function() {
@@ -8,5 +13,25 @@ bnt.addEventListener('click', function() {
 		hidden.style.display = 'none';
 	} else {
 		hidden.style.display = 'block';
-	}		 
-});	
+	}
+		 
+});
+
+bntform.addEventListener('click',function(){
+
+	if(form.style.display === 'none'){
+		form.style.display = 'block'
+	} else {
+		form.style.display = 'none'
+	}
+});
+
+bntform.addEventListener('click', function(){
+
+	if(discussoes.style.display === 'block'){
+		discussoes.style.display = 'none'
+	} else {
+		discussoes.style.display = 'block'
+	}
+})
+
