3e2cb71f18a1e11fa1cae5ad9ae70c75d71ca57c
Khemyli Mendonça <65932172+Khemyli@users.noreply.github.com>
2021-07-30 20:44:34 -0300

    fourth commit
    final

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-	var bnt = document.querySelector('#mostrar');
-	var hidden = document.querySelector('.hidden');
-	var bntform = document.querySelector('#botao-discussoes')
-	var form = document.querySelector('#form')
-	var discussoes = document.querySelector('#discussoes')
-	var bntenviar = document.querySelector('#botao-form')
-	
+var bnt = document.querySelector('#mostrar');
+var hidden = document.querySelector('.hidden');
+var bntform = document.querySelector('#botao-discussoes')
+var form = document.querySelector('#form')
+var discussoes = document.querySelector('#discussoes')
+var bntenviar = document.querySelector('#botao-form')
+var enviaddos = document.querySelector('.enviado')
+var bntnovotopico = document.querySelector('#novotopico')
 
 
+/*ver mais*/
 bnt.addEventListener('click', function() {
 
-	if(hidden.style.display === 'block') {
-		hidden.style.display = 'none';
-	} else {
-		hidden.style.display = 'block';
-	}
-		 
+if(hidden.style.display === 'block') {
+	hidden.style.display = 'none';
+} else {
+	hidden.style.display = 'block';
+}
+	 
 });
 
-bntform.addEventListener('click',function(){
+/*aparecer formulário*/
+bntform.addEventListener('click', function exibir(){
+	document.getElementById("form").style.display = "block";
+	document.getElementById("discussoes").style.display = "none";
 
-	if(form.style.display === 'none'){
-		form.style.display = 'block'
-	} else {
-		form.style.display = 'none'
-	}
 });
 
-bntform.addEventListener('click', function(){
+/*sair do formulário para criar novo topico*/
 
-	if(discussoes.style.display === 'block'){
-		discussoes.style.display = 'none'
-	} else {
-		discussoes.style.display = 'block'
-	}
+bntenviar.addEventListener('click', function(){
+	document.getElementById("enviado").style.display = "block";
+	document.getElementById("discussoes").style.display = "none";
+	document.getElementById("form").style.display = "none";
+	document.getElementById("hiddenperguntas").style.display = "none";
+	
+});
+
+bntnovotopico.addEventListener('click', function(){
+	document.getElementById("form").style.display = "block"
+	document.getElementById("enviado").style.display = "none"
+	document.getElementById("hiddenperguntas").style.display = "block";
 })
-
